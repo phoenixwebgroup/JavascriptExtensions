964e6b64dd9d45f7a7fbd8a24061db9e59a257ae
Wesley McClure <wmcclure@phoenixwebgroup.com>
2011-07-09 16:46:23 -0500

    Implement swap cash settlements

---
+++ Financial/src/trades/Swap.js
@@ -0,0 +1,23 @@
+﻿financial.trades.Swap = function () {
+	this.CustomerWill = financial.trades.CustomerWill.BUY;
+	this.FixedPrice = 2.00;
+	this.Quantity = 10000;
+
+	var swap = this;
+
+	this.GetCashSettlement = function (settlementPrice) {
+		var settlement = new financial.trades.CashSettlement();
+		if (settlementPrice == swap.FixedPrice) {
+			settlement.Amount = 0;
+			settlement.Explanation = 'Bought and sold at same price';
+			return settlement;
+		}
+		var initiallyLong = swap.CustomerWill == financial.trades.CustomerWill.BUY;
+		var soldAt = initiallyLong ? settlementPrice : swap.FixedPrice;
+		var boughtAt = initiallyLong ? swap.FixedPrice : settlementPrice;
+
+		settlement.Amount = (soldAt - boughtAt) * swap.Quantity;
+		settlement.Explanation = '(Sold At - Bought At) * Quantity = (' + soldAt + ' - ' + boughtAt + ') * ' + swap.Quantity;
+		return settlement;
+	};
+}
---
+++ Financial/src/trades/SwapSpec.js
@@ -0,0 +1,81 @@
+﻿describe("financial.trades.Swap", function () {
+
+	describe("GetCashSettlement", function () {
+		var swap = new financial.trades.Swap();
+
+		beforeEach(function () {
+			swap.FixedPrice = 2;
+			swap.Quantity = 1000;
+		});
+
+		describe("buy initially", function () {
+
+			beforeEach(function () {
+				swap.CustomerWill = financial.trades.CustomerWill.BUY;
+			});
+
+			it("should be worthless when the settlement price is equal to the fixed price", function () {
+
+				var settlement = swap.GetCashSettlement(swap.FixedPrice);
+
+				expect(settlement).toEqual({
+					Amount: 0,
+					Explanation: 'Bought and sold at same price'
+				});
+			});
+
+			it("should be a gain if the market goes up", function () {
+				var settlement = swap.GetCashSettlement(swap.FixedPrice + 1);
+
+				expect(settlement).toEqual({
+					Amount: 1000,
+					Explanation: '(Sold At - Bought At) * Quantity = (3 - 2) * 1000'
+				});
+			});
+
+			it("should be a loss if the market goes down", function () {
+				var settlement = swap.GetCashSettlement(swap.FixedPrice - 1);
+
+				expect(settlement).toEqual({
+					Amount: -1000,
+					Explanation: '(Sold At - Bought At) * Quantity = (1 - 2) * 1000'
+				});
+			});
+		});
+
+		describe("sell initially", function () {
+
+			beforeEach(function () {
+				swap.CustomerWill = financial.trades.CustomerWill.SELL;
+			});
+
+			it("should be worthless when the settlement price is equal to the fixed price", function () {
+
+				var settlement = swap.GetCashSettlement(swap.FixedPrice);
+
+				expect(settlement).toEqual({
+					Amount: 0,
+					Explanation: 'Bought and sold at same price'
+				});
+			});
+
+			it("should be a gain if the market goes down", function () {
+				var settlement = swap.GetCashSettlement(swap.FixedPrice - 1);
+
+				expect(settlement).toEqual({
+					Amount: 1000,
+					Explanation: '(Sold At - Bought At) * Quantity = (2 - 1) * 1000'
+				});
+			});
+
+			it("should be a loss if the market goes up", function () {
+				var settlement = swap.GetCashSettlement(swap.FixedPrice + 1);
+
+				expect(settlement).toEqual({
+					Amount: -1000,
+					Explanation: '(Sold At - Bought At) * Quantity = (2 - 3) * 1000'
+				});
+			});
+		});
+	});
+});
